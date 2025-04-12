12e7acc167ec9c41ef1b4b62ab419d40e537603f
kadelcode <kadelcode@gmail.com>
2025-04-12 18:22:13 +0100

    Update hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Hero = () => {
     return (
-        <section className="relative flex flex-col items-center justify-center h-screen bg-gradient-to-r from-yellow-300 to-orange-500 text-white">
+        <section className="relative flex flex-col items-center px-5 justify-center h-screen text-white">
             <div className="text-center space-y-6">
-                <h1 className="text-5xl font-extrabold">Power Your Future with Solar Energy</h1>
+                <h1 className="text-4xl sm:text-5xl font-extrabold">Power Your Future with Solar Energy</h1>
                 <p className="text-lg">Sustainable. Affordable. Efficient</p>
                 <div className="relative w-full h-full flex justify-center">
                     <Image
