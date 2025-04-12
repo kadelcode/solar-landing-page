5e78f1ba2f0a06af558826a78dbb5aa791533e0f
kadelcode <kadelcode@gmail.com>
2025-04-12 23:39:38 +0100

    Update styles

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const Hero = () => {
     return (
-        <section className="relative flex flex-col items-center px-5 justify-center h-screen text-white">
+        <section className="flex flex-col items-center bg-[#FFF] px-5 py-20 justify-center text-[#000]">
             <div className="text-center space-y-6">
                 <h1 className="text-4xl sm:text-5xl font-extrabold">Power Your Future with Solar Energy</h1>
                 <p className="text-lg">Sustainable. Affordable. Efficient</p>
-                <div className="relative w-full h-full flex justify-center">
+                <div className="relative w-full h-full flex items-center justify-center">
                     <Image
                       src="/images/solar-energy.jpeg"
                       alt="Solar Energy"
@@ -15,7 +15,7 @@ const Hero = () => {
                       className="rounded-lg"
                     />
                 </div>
-                <button className="px-6 py-3 bg-[#1d293d] text-[#fff] rounded-lg mt-6">Get a Free Quote</button>
+                <button className="px-6 py-3 bg-[oklch(79.5% 0.184 86.047)] text-[#fff] rounded-lg mt-6">Get a Free Quote</button>
             </div>
         </section>
     );
