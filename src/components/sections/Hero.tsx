b0c03f674d27f0310b3df43793c72ee598d96925
kadelcode <kadelcode@gmail.com>
2025-04-13 01:03:46 +0100

    Add new styles

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,9 +4,9 @@ const Hero = () => {
     return (
         <section className="flex flex-col items-center bg-[#FFF] px-5 py-20 justify-center text-[#000]">
             <div className="text-center space-y-6">
-                <h1 className="text-4xl sm:text-5xl font-extrabold">Power Your Future with Solar Energy</h1>
+                <h1 className="text-4xl sm:text-5xl font-extrabold">Power Your Future with <span className="text-sky-500">Solar Energy</span></h1>
                 <p className="text-lg">Sustainable. Affordable. Efficient</p>
-                <div className="relative w-full h-full flex items-center justify-center">
+                <div className="relative z-20 w-full h-full flex items-center justify-center">
                     <Image
                       src="/images/solar-energy.jpeg"
                       alt="Solar Energy"
