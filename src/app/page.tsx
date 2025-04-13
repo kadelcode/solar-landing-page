06e3683af1023c680aeaf909d98364f5d8eee697
kadelcode <kadelcode@gmail.com>
2025-04-13 08:59:10 +0100

    Add services component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/sections/Hero";
+import Services from "@/components/sections/Services";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Services />
     </div>
   );
 }
